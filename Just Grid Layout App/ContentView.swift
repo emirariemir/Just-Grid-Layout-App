66dfccedd65e6b58739540293f0251afaabc28bc
Emir Arı <emirariomatic@gmail.com>
2022-08-05 15:57:33 +0300

    Complete UI template and JSON decoding

--- Just Grid Layout App/ContentView.swift
+++ Just Grid Layout App/ContentView.swift
@@ -7,10 +7,68 @@
 
 import SwiftUI
 
+class GridViewModel: ObservableObject {
+    @Published var items = 0..<10
+    
+    @Published var results = [Result]()
+    
+    init() {
+        guard let url = URL(string: "https://rss.applemarketingtools.com/api/v2/us/music/most-played/25/albums.json") else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, resp, err in
+            guard let data = data else { return }
+            do {
+                let rss = try JSONDecoder().decode(RSS.self, from: data)
+                self.results = rss.feed.results
+            } catch {
+                print("Failed while decoding: \(error)")
+            }
+        }.resume()
+    }
+}
+
+struct RSS: Decodable {
+    var feed: Feed
+}
+
+struct Feed: Decodable {
+    var results: [Result]
+}
+
+struct Result: Decodable, Hashable {
+    var artistName, name, artworkUrl100, releaseDate: String
+}
+
 struct ContentView: View {
+    @ObservedObject var vm = GridViewModel()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ScrollView {
+                // NOTE THIS
+                LazyVGrid(columns: [
+                    GridItem(.flexible(minimum: 100, maximum: 200), spacing: 12, alignment: .top),
+                    GridItem(.flexible(minimum: 100, maximum: 200), spacing: 12, alignment: .top),
+                    GridItem(.flexible(minimum: 100, maximum: 200), alignment: .top)
+                ], spacing: 12, content: {
+                    ForEach(vm.results, id: \.self) { app in
+                        VStack (alignment: .leading) {
+                            Spacer()
+                                .frame(width: 100, height: 100)
+                                .background(Color.blue)
+                            Text(app.name)
+                                .font(.system(size: 12, weight: .semibold))
+                            Text(app.artistName)
+                                .font(.system(size: 11, weight: .regular))
+                            Text(app.releaseDate)
+                                .font(.system(size: 11, weight: .regular))
+                        }
+                        .padding()
+                        //.background(Color.green)
+                    }
+                }).padding(.horizontal, 12)
+            }.navigationTitle("Music Layout")
+        }
     }
 }
 
