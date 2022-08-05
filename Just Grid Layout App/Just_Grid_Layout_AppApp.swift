216d2d0dd75793c79779ef9301e19f821e212ba2
Emir Arı <emirariomatic@gmail.com>
2022-08-05 13:22:39 +0300

    Initial Commit

---
+++ Just Grid Layout App/Just_Grid_Layout_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Just_Grid_Layout_AppApp.swift
+//  Just Grid Layout App
+//
+//  Created by Emir on 5.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct Just_Grid_Layout_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
